121bc731ada611c658b16f2a1b47f29cc88035e3
Ratul Minhaz <minhaz.ratul@gmail.com>
2015-08-16 01:06:34 +0600

    Created contact.js

---
+++ contact.js
@@ -0,0 +1,10 @@
+var Contact = {}
+
+Contact.parseName = function (str) {
+    // str is a string containing the name and phone number separated by comma
+    // extract and return the name from the str argument
+    // str will always contain only one comma character
+    return str.split(',')[0];
+}
+
+module.exports = Contact
