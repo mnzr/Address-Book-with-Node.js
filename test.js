034efa2d1be6330f8048d51e9f0bf7b3bc9e073e
Ratul Minhaz <minhaz.ratul@gmail.com>
2015-08-16 01:23:35 +0600

    Tested contact.js

--- test.js
+++ test.js
@@ -6,5 +6,7 @@ var MyParrot = require('./parrot')
 // console.log( MyParrot.speakSpanish() )
 
 var name = Contact.parseName("John Smith,604-123-9090")
-console.log(name) // -> should print "John Smith"
+var number = Contact.parseNumber("John Smith,604-123-9090")
+
+console.log(number) // -> should print "John Smith"
 
